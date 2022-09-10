73820156fa9aee91716acf688f26966e18971044
GaryDavis <davisga1999@gmail.com>
2022-09-10 00:01:55 -0500

    commit tree 2ae403908c0688d28784573b53a2d63280abd2b1

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(x){
+    return Math.abs(x - 42)
+}
+function distanceFromHqInFeet(y){
+    return (distanceFromHqInBlocks(y) * 264)
+}
+function distanceTravelledInFeet(a, b){
+    return (Math.abs(a - b) * 264)
+}
+function calculatesFarePrice(a, b){
+    let fare = (distanceTravelledInFeet(a, b))
+    if (fare < 400){
+        return 0
+    } else if (fare >= 400, fare <= 2000){
+        return ((fare - 400) * 2 / 100)
+    } else if (fare > 2000, fare < 2500){
+        return 25
+    } else if (fare > 2500){
+        return 'cannot travel that far'
+    }
+}
